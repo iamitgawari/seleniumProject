6a30c636769144696f0229572cdb4a7692263104
Archana <archana.gawari@fafadiatech.com>
2022-09-18 10:34:42 +0530

    getwehyrHvvvsv

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wohfdsy4hwred!");
+        System.out.println("Hello wohfdsrry4hwred!");
     }
 }
