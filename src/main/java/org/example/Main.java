885c59546b767599d3c44ef9e071a8aebf216a21
Amit <iamit.gawari@gmail.com>
2022-09-18 10:57:15 +0530

    check

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wohfdsyhfdhrry4hwdsgred!");
+        System.out.println("Hello wohy4hwdsgred!");
     }
 }
